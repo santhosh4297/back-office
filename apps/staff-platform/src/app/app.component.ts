f665a0b22fa6c793ea1b62afd0a63db13b07c9bc
Santhosh Mittapally <santhosh.mittapally@tide.co>
2022-08-19 19:14:23 +0530

    okta integration changes

--- apps/staff-platform/src/app/app.component.ts
+++ apps/staff-platform/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { filter, map, Observable, shareReplay } from 'rxjs';
+import { OKTA_AUTH, OktaAuthStateService } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'back-office-root',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'staff-platform';
+  public isAuthenticated$: Observable<boolean> = this.oktaStateService.authState$
+      .pipe(
+          filter(authState => !!authState),
+          map(authState => authState.isAuthenticated ?? false),
+          shareReplay()
+      );
+  
+  public name$: Observable<string> = this.oktaStateService.authState$
+      .pipe(
+          filter(authState => !!authState && !!authState.isAuthenticated),
+          map(authState => authState.idToken?.claims.name ?? '')
+      );
+
+  constructor(private oktaStateService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+   }
+
+  public async signIn(): Promise<void> {
+    await this.oktaAuth.signInWithRedirect();
+  }
+
+  public async signOut(): Promise<void> {
+    await this.oktaAuth.signOut();
+  }
 }
--- apps/staff-platform/src/app/app.module.ts
+++ apps/staff-platform/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import {
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent,
+} from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://tide.okta-emea.com',
+  clientId: '0oa8hkj0rd6Num2Wl0i7',
+  redirectUri: window.location.origin + '/login/callback',
+  scopes: ['openid', 'profile', 'email'],
+});
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, LoginComponent],
   imports: [
+    HttpClientModule,
+    OktaAuthModule,
     BrowserModule,
     RouterModule.forRoot(
       [
@@ -20,11 +38,21 @@ import { RouterModule } from '@angular/router';
           path: '',
           component: NxWelcomeComponent,
         },
+        {
+          path: 'login',
+          component: LoginComponent
+        },
+        { path: 'login/callback', component: OktaCallbackComponent },
       ],
       { initialNavigation: 'enabledBlocking' }
     ),
   ],
-  providers: [],
+  providers: [
+    { 
+      provide: OKTA_CONFIG, 
+      useValue: { oktaAuth } 
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
